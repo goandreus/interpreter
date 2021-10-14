b5effa572e029efe8dfff67321ada6fa340d2302
goandreus <andres.chavez.crisostomo@gmail.com>
2021-10-13 21:22:24 -0500

    lexer test

---
+++ tests/lexer_test.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+from typing import List
+
+from lpp.token import (
+    Token,
+    TokenType,
+)
+from lpp.lexer import Lexer
+
+
+class LexerTest(TestCase):
+
+    def test_illegal(self) -> None:
+        source: str = '¡¿@'
+        lexer: Lexer = Lexer(source)
+
+        tokens: List[Token] = []
+        for i in range(len(source)):
+            tokens.append(lexer.next_token())
+
+        expected_tokens: List[Token] = [
+            Token(TokenType.ILLEGAL, '¡'),
+            Token(TokenType.ILLEGAL, '¿'),
+            Token(TokenType.ILLEGAL, '@'),
+        ]
+
+        self.assertEquals(tokens, expected_tokens)
