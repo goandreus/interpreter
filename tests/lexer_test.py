430266981c19b0a1ec7f7549d83bdd7a8a1d1f4b
goandreus <andres.chavez.crisostomo@gmail.com>
2021-11-15 10:37:57 -0500

    ad test function

--- tests/lexer_test.py
+++ tests/lexer_test.py
@@ -11,7 +11,7 @@ from lpp.lexer import Lexer
 class LexerTest(TestCase):
 
     def test_illegal(self) -> None:
-        source: str = '¡¿@'
+        source: str = '@¡¿'
         lexer: Lexer = Lexer(source)
 
         tokens: List[Token] = []
@@ -19,9 +19,132 @@ class LexerTest(TestCase):
             tokens.append(lexer.next_token())
 
         expected_tokens: List[Token] = [
+            Token(TokenType.ILLEGAL, '@'),
             Token(TokenType.ILLEGAL, '¡'),
             Token(TokenType.ILLEGAL, '¿'),
-            Token(TokenType.ILLEGAL, '@'),
+        ]
+
+        self.assertEquals(tokens, expected_tokens)
+
+    def test_one_character_operators(self) -> None:
+        source: str = '=+'
+        lexer: Lexer = Lexer(source)
+
+        tokens: List[Token] = []
+        for i in range(len(source)):
+            tokens.append(lexer.next_token())
+
+        expected_tokens: List[Token] = [
+            Token(TokenType.ASSIGN, '='),
+            Token(TokenType.PLUS, '+'),
+        ]
+
+        self.assertEquals(tokens, expected_tokens)
+
+    def test_eof(self) -> None:
+        source: str = '+'
+        lexer: Lexer = Lexer(source)
+
+        tokens: List[Token] = []
+        for i in range(len(source) + 1):
+            tokens.append(lexer.next_token())
+
+        expected_tokens: List[Token] = [
+            Token(TokenType.PLUS, '+'),
+            Token(TokenType.EOF, ''),
+        ]
+
+        self.assertEquals(tokens, expected_tokens)
+
+    def test_delimiters(self) -> None:
+        source: str = '(){},;'
+        lexer: Lexer = Lexer(source)
+
+        tokens: List[Token] = []
+        for i in range(len(source)):
+            tokens.append(lexer.next_token())
+
+        expected_tokens: List[Token] = [
+            Token(TokenType.LPAREN, '('),
+            Token(TokenType.RPAREN, ')'),
+            Token(TokenType.LBRACE, '{'),
+            Token(TokenType.RBRACE, '}'),
+            Token(TokenType.COMMA, ','),
+            Token(TokenType.SEMICOLON, ';'),
+        ]
+
+        self.assertEquals(tokens, expected_tokens)
+
+    def test_assignment(self) -> None:
+        source: str = 'variable cinco = 5;'
+        lexer: Lexer = Lexer(source)
+
+        tokens: List[Token] = []
+        for i in range(5):
+            tokens.append(lexer.next_token())
+
+        expected_tokens: List[Token] = [
+            Token(TokenType.LET, 'variable'),
+            Token(TokenType.IDENT, 'cinco'),
+            Token(TokenType.ASSIGN, '='),
+            Token(TokenType.INT, '5'),
+            Token(TokenType.SEMICOLON, ';'),
+        ]
+
+        self.assertEquals(tokens, expected_tokens)
+
+    def test_function_declaration(self) -> None:
+        source: str = '''
+            variable suma = procedimiento(x, y) {
+                x + y;
+            };
+        '''
+        lexer: Lexer = Lexer(source)
+
+        tokens: List[Token] = []
+        for i in range(16):
+            tokens.append(lexer.next_token())
+
+        expected_tokens: List[Token] = [
+            Token(TokenType.LET, 'variable'),
+            Token(TokenType.IDENT, 'suma'),
+            Token(TokenType.ASSIGN, '='),
+            Token(TokenType.FUNCTION, 'procedimiento'),
+            Token(TokenType.LPAREN, '('),
+            Token(TokenType.IDENT, 'x'),
+            Token(TokenType.COMMA, ','),
+            Token(TokenType.IDENT, 'y'),
+            Token(TokenType.RPAREN, ')'),
+            Token(TokenType.LBRACE, '{'),
+            Token(TokenType.IDENT, 'x'),
+            Token(TokenType.PLUS, '+'),
+            Token(TokenType.IDENT, 'y'),
+            Token(TokenType.SEMICOLON, ';'),
+            Token(TokenType.RBRACE, '}'),
+            Token(TokenType.SEMICOLON, ';'),
+        ]
+
+        self.assertEquals(tokens, expected_tokens)
+
+    def test_function_call(self) -> None:
+        source: str = 'variable resultado = suma(dos, tres);'
+        lexer: Lexer = Lexer(source)
+
+        tokens: List[Token] = []
+        for i in range(10):
+            tokens.append(lexer.next_token())
+
+        expected_tokens: List[Token] = [
+            Token(TokenType.LET, 'variable'),
+            Token(TokenType.IDENT, 'resultado'),
+            Token(TokenType.ASSIGN, '='),
+            Token(TokenType.IDENT, 'suma'),
+            Token(TokenType.LPAREN, '('),
+            Token(TokenType.IDENT, 'dos'),
+            Token(TokenType.COMMA, ','),
+            Token(TokenType.IDENT, 'tres'),
+            Token(TokenType.RPAREN, ')'),
+            Token(TokenType.SEMICOLON, ';'),
         ]
 
         self.assertEquals(tokens, expected_tokens)
