2ecec7f7ac7722c0473bb706d02e23aed8310280
goandreus <andres.chavez.crisostomo@gmail.com>
2021-11-15 10:37:33 -0500

    token Dict

--- lpp/token.py
+++ lpp/token.py
@@ -3,7 +3,10 @@ from enum import (
     Enum,
     unique,
 )
-from typing import NamedTuple
+from typing import (
+    Dict,
+    NamedTuple
+)
 
 
 @unique
@@ -19,8 +22,8 @@ class TokenType(Enum):
     LET = auto()
     LPAREN = auto()
     PLUS = auto()
-    RBRACE = auto()
     RPAREN = auto()
+    RBRACE = auto()
     SEMICOLON = auto()
 
 
@@ -30,3 +33,12 @@ class Token(NamedTuple):
 
     def __str__(self) -> str:
         return f'Type: {self.token_type}, Literal: {self.literal}'
+
+
+def lookup_token_type(literal: str) -> TokenType:
+    keywords: Dict[str, TokenType] = {
+        'procedimiento': TokenType.FUNCTION,
+        'variable': TokenType.LET
+    }
+
+    return keywords.get(literal, TokenType.IDENT)
